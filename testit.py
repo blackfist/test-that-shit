e71e936200f8e3912c1663d15bd01f9eb94e5a1e
Kevin Thompson <kevin.david.thompson@gmail.com>
2013-09-24 22:35:01 -0500

    Autocommit

---
+++ testit.py
@@ -0,0 +1,12 @@
+from git import *
+import uuid
+
+repo = Repo('.')
+shiz = str(uuid.uuid4()).upper() + '.json'
+
+outfile = open(shiz,'w')
+outfile.write(shiz)
+outfile.close()
+
+repo.git.add('.')
+repo.git.commit('-a', '-m Autocommit')
